2d7cc56fb43931581b0f0db6eda92d5726100918
Jai Shewaramani <jai.shewaramani1@hotmail.com>
2020-02-04 12:26:40 +0530

    Add project files.

---
+++ EntityFrameworkDemo/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EntityFrameworkDemo.Models
+{
+    public class Student
+    {
+        [Key]
+        public int studentId { get; set; }
+        public string studentName { get; set; }
+        public string studentCourse { get; set; }
+        public int studentFees { get; set; }
+    }
+}
